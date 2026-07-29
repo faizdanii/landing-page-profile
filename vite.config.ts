d6b17e8a751d64bbc46436dc01bc1a2d04e69edb
Faiz <faiz.danii@iconpln.co.id>
2026-07-29 10:30:42 +0700

    feat: configure GitHub Actions deployment pipeline and set base path for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/landing-page-profile/',
   plugins: [
     vue(),
     vueDevTools(),
